28171216acafde4df241ddf3f8c8e7c3c9bc0c8d
Sam Harwell <sam@tunnelvisionlabs.com>
2016-10-08 16:32:45 -0500

    Use existing Arrays.binarySearch method

--- src/atn/ArrayPredictionContext.ts
+++ src/atn/ArrayPredictionContext.ts
@@ -31,48 +31,13 @@
 // ConvertTo-TS run at 2016-10-04T11:26:24.9093131-07:00
 
 import { Array2DHashSet } from '../misc/Array2DHashSet';
+import { Arrays } from '../misc/Arrays';
 import { JavaSet, NotNull, Override } from '../misc/Stubs';
 import { PredictionContext } from './PredictionContext';
 import { PredictionContextCache } from './PredictionContextCache';
 import { SingletonPredictionContext } from './SingletonPredictionContext';
 
 import * as assert from 'assert';
-
-/**
- * Searches the specified array of numbers for the specified value using the binary search algorithm. The array must be
- * sorted prior to making this call. If it is not sorted, the results are unspecified. If the array contains multiple
- * elements with the specified value, there is no guarantee which one will be found.
- *
- * @return index of the search key, if it is contained in the array; otherwise, (-(insertion point) - 1). The insertion
- * point is defined as the point at which the key would be inserted into the array: the index of the first element
- * greater than the key, or array.length if all elements in the array are less than the specified key. Note that this
- * guarantees that the return value will be >= 0 if and only if the key is found.
- */
-function binarySearch(array: number[], key: number): number {
-	return binarySearch0(array, 0, array.length, key);
-}
-
-function binarySearch0(array: number[], fromIndex: number, toIndex: number, key: number): number {
-	let low: number = fromIndex;
-	let high: number = toIndex - 1;
-
-	while (low <= high) {
-		let mid: number = (low + high) >>> 1;
-		let midVal: number = array[mid];
-
-		if (midVal < key) {
-			low = mid + 1;
-		} else if (midVal > key) {
-			high = mid - 1;
-		} else {
-			// key found
-			return mid;
-		}
-	}
-
-	// key not found.
-	return -(low + 1);
-}
 
 export class ArrayPredictionContext extends PredictionContext {
 	@NotNull
@@ -102,7 +67,7 @@ export class ArrayPredictionContext extends PredictionContext {
 
 	@Override
 	findReturnState(returnState: number): number {
-		return binarySearch(this.returnStates, returnState);
+		return Arrays.binarySearch(this.returnStates, returnState);
 	}
 
 	@Override
