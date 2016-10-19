1e3fa41f7c69330df80aebb3c648dc7a8f562f08
Sam Harwell <sam@tunnelvisionlabs.com>
2016-10-19 05:59:40 -0500

    Remove Stub_BitSet.ts
    This file was unintentionally added back during an earlier merge commit.

--- src/misc/Stub_BitSet.ts
+++
@@ -1,9 +0,0 @@
-import { Equatable } from './Stubs';
-
-export class BitSet implements Equatable {
-	get(alt: number): boolean { throw new Error("Not implemented"); }
-	set(alt: number) { throw new Error("Not implemented"); }
-	clone(): BitSet { throw new Error("Not implemented"); }
-	hashCode(): number { throw new Error("Not implemented"); }
-	equals(obj: any): boolean { throw new Error("Not implemented"); }
-}
