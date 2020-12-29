851b0202da271ea7530b60f5ceb75f84574dd661
Sam Harwell <sam@tunnelvisionlabs.com>
2020-12-29 15:33:04 -0800

    Incorporate changes through the 4.9 release

--- src/tree/ParseTreeWalker.ts
+++ src/tree/ParseTreeWalker.ts
@@ -12,6 +12,14 @@ import { RuleNode } from "./RuleNode";
 import { ParserRuleContext } from "../ParserRuleContext";
 
 export class ParseTreeWalker {
+	/**
+	 * Performs a walk on the given parse tree starting at the root and going down recursively
+	 * with depth-first search. On each node, {@link ParseTreeWalker#enterRule} is called before
+	 * recursively walking down into child nodes, then
+	 * {@link ParseTreeWalker#exitRule} is called after the recursive call to wind up.
+	 * @param listener The listener used by the walker to process grammar rules
+	 * @param t The parse tree to be walked on
+	 */
 	public walk<T extends ParseTreeListener>(listener: T, t: ParseTree): void {
 		let nodeStack: ParseTree[] = [];
 		let indexStack: number[] = [];
@@ -72,10 +80,10 @@ export class ParseTreeWalker {
 	}
 
 	/**
-	 * The discovery of a rule node, involves sending two events: the generic
-	 * {@link ParseTreeListener#enterEveryRule} and a
-	 * {@link RuleContext}-specific event. First we trigger the generic and then
-	 * the rule specific. We to them in reverse order upon finishing the node.
+	 * Enters a grammar rule by first triggering the generic event {@link ParseTreeListener#enterEveryRule}
+	 * then by triggering the event specific to the given parse tree node
+	 * @param listener The listener responding to the trigger events
+	 * @param r The grammar rule containing the rule context
 	 */
 	protected enterRule(listener: ParseTreeListener, r: RuleNode): void {
 		let ctx = r.ruleContext as ParserRuleContext;
@@ -86,6 +94,12 @@ export class ParseTreeWalker {
 		ctx.enterRule(listener);
 	}
 
+	/**
+	 * Exits a grammar rule by first triggering the event specific to the given parse tree node
+	 * then by triggering the generic event {@link ParseTreeListener#exitEveryRule}
+	 * @param listener The listener responding to the trigger events
+	 * @param r The grammar rule containing the rule context
+	 */
 	protected exitRule(listener: ParseTreeListener, r: RuleNode): void {
 		let ctx = r.ruleContext as ParserRuleContext;
 		ctx.exitRule(listener);
